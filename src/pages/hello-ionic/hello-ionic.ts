b378de0b188145eb639a28c6702f05af68b573cf
Lalit Dogra <lalitdogra04@gmail.com>
2017-03-31 17:59:30 +0530

    ionic2 HomePage

---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,50 @@
+import { Component , ViewChild} from '@angular/core';
+import { Slides } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { NavController } from 'ionic-angular';
+import { PostDetail } from '../post-detail/post-detail';
+
+@Component({
+	selector: 'page-hello-ionic',
+	templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+	@ViewChild(Slides) slides: Slides;
+	constructor(private http: Http,public nav: NavController) {
+	}
+	/*slideChanged() {
+		let currentIndex = this.slides.getActiveIndex();
+		console.log("Current index is", currentIndex);
+	}*/
+	/*public homeOptions = {
+		initialSlide: 0,
+		loop: true,
+		noSwiping: false,
+		autoplayDisableOnInteraction: false
+	};*/
+	public Data :any= [
+	{ img1:"assets/images/slide1.jpeg"},
+	{ img1:"assets/images/slide2.jpeg"},
+	{ img1:"assets/images/slide3.jpeg"},
+	{ img1:"assets/images/slide4.jpeg"},
+	
+	];
+
+	url: string = 'http://uniquecoders.in/dev/videogallery/wp-json/wp/v2/posts';
+	items: any;	
+
+	ionViewDidEnter() {
+		this.http.get( this.url )
+		.map(res => res.json())
+		.subscribe(data => {
+			this.items = data;
+		});
+	}
+	itemTapped(event, item) {
+		this.nav.push(PostDetail, {
+		  item: item
+		});
+	}
+
+}
